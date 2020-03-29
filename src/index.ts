14184956a9d07735cfa7cc39afde9cc2d633c873
Andy <afrith.denver.usa@gmail.com>
2020-03-29 11:54:25 -0600

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import * as bodyParser from "body-parser";
-import * as cors from "cors";
-import * as express from "express";
-import { ENV } from "./env";
-import { logger } from "./logger";
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+import * as express from 'express';
+import { ENV } from './env';
+import { logger } from './logger';
 
 (async () => {
-  logger.info("tmdb-demo service starting", { ENV });
+  logger.info('tmdb-demo service starting', { ENV });
   const app = express();
   app.use(cors());
   app.use(bodyParser.json());
 
-  app.use("/healthy", async (req, res) => {
+  app.use('/healthy', async (req, res) => {
     res.send({
-      message: "tbdb-demo is ok"
+      message: 'tbdb-demo is ok',
     });
   });
 
-  app.listen({ port: ENV.RUN_PORT }, () => {
-    logger.info(`tbdb-demo listening at :${ENV.RUN_PORT}...`);
+  app.listen({ port: ENV.PORT }, () => {
+    logger.info(`tbdb-demo listening at :${ENV.PORT}...`);
   });
 })();
 
